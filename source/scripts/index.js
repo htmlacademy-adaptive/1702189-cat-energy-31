1afb1ae871da7c3e6fe61805cbce040407f4c959
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-05-21 23:05:40 +0300

    Добавляет стилизацию и скрипт для выпадающего меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const button = document.querySelector('.main-navigation__toggle');
+const menu = document.querySelector('.main-navigation');
+const body = document.querySelector('.page__body');
+
+body.classList.remove('nojs');
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('main-navigation--opened');
+});
